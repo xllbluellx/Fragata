e7916da6f864468509afa4d3b4e47a6fc074f341
martinalanis <martinalanis.dev@gmail.com>
2016-10-30 18:47:38 -0600

    Cambios de prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel de Danix :)
                 </div>
 
                 <div class="links">
